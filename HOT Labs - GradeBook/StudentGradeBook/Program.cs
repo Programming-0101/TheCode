9c330c05a30f67f91fd2e5cdb9a345b680b7919c
dagilleland <dagilleland@shaw.ca>
2016-11-22 09:53:37 -0700

    Save File

--- HOT Labs - GradeBook/StudentGradeBook/Program.cs
+++ HOT Labs - GradeBook/StudentGradeBook/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StudentGradeBook.ConsoleUI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,7 +17,39 @@ namespace StudentGradeBook
 
         public void Run()
         {
-            throw new NotImplementedException();
+            string userInput;
+            do
+            {
+                DisplayMenu();
+                Console.Write("Select an option from the menu: ");
+                userInput = Console.ReadLine().ToUpper();
+                try
+                {
+                    HandleMenuOption(userInput);
+                }
+                catch (AbortFormInputException ex)
+                {
+
+                }
+                catch (Exception ex)
+                {
+                    
+                }
+            } while (userInput != "Q");
+        }
+
+        private void HandleMenuOption(string option)
+        {
+            switch (option)
+            {
+                case "1":
+                    GradebookForm form = new GradebookForm();
+                    var gradebook = form.PromptForData();
+                    SaveGrades(gradebook);
+                    break;
+                case "2":
+                    break;
+            }
         }
 
         private void DisplayMenu()
@@ -26,6 +59,44 @@ namespace StudentGradeBook
             Console.WriteLine("1) Create Gradebook File");
             Console.WriteLine("2) Load Gradebook File");
             Console.WriteLine("Q) Quit Program");
+            Console.WriteLine();
+        }
+
+        private void SaveGrades(StudentEvaluation gradebook)
+        {
+            /* File structure:
+             * CourseName, CourseNumber, GroupCount
+             * GroupName, GroupWeight, PassMark, ItemCount
+             * ItemName, ItemWeight, PossibleMarks, EarnedMark
+             */
+            List<string> lines = new List<string>();
+            string[] groupNames = gradebook.ListEvaluationGroups();
+            lines.Add($"{gradebook.CourseName},{gradebook.CourseNumber},{groupNames.Length}");
+            foreach (string name in groupNames)
+            {
+                var group = gradebook.GetEvaluationGroup(name);
+                var itemNames = group.ListGroupComponents();
+                lines.Add($"{group.Name},{group.Weight},{group.PassMark},{itemNames.Count}");
+                foreach (string item in itemNames)
+                {
+                    var component = group.GetEvaluationItem(item);
+                    lines.Add($"{component.Name},{component.Weight},{component.PossibleMarks},{component.EarnedMark}");
+                }
+            }
+            CSVFileIO writer = new CSVFileIO($"{gradebook.CourseNumber}.txt");
+            writer.WriteAllLines(lines, false);
+        }
+
+        private StudentEvaluation LoadGrades(string fileName)
+        {
+            /* File structure:
+             * CourseName, CourseNumber, GroupCount
+             * GroupName, GroupWeight, PassMark, ItemCount
+             * ItemName, ItemWeight, PossibleMarks, EarnedMark
+             */
+            StudentEvaluation gradebook = null;
+
+            return gradebook;
         }
     }
 }
