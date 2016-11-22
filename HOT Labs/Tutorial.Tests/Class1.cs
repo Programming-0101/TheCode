a17aa61572794b977a5ef74c18b14b468159b1f0
dagilleland <dagilleland@shaw.ca>
2016-11-22 05:39:27 -0700

    Samples - MemoryAddress, Color

---
+++ HOT Labs/Tutorial.Tests/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tutorial.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void should()
+        {
+            byte max = (byte)255;
+            const byte base16 = (byte)16;
+            string result = HexDigit((byte)(max / base16)) + HexDigit((byte)(max % base16));
+            Assert.True(result.Equals("FF"));
+        }
+        private static string HexDigit(byte number)
+        {
+            string result;
+            if (number < 10)
+                result = number.ToString();
+            else if (number == 10)
+                result = "A";
+            else if (number == 11)
+                result = "B";
+            else if (number == 12)
+                result = "C";
+            else if (number == 13)
+                result = "D";
+            else if (number == 14)
+                result = "E";
+            else if (number == 15)
+                result = "F";
+            else
+                result = "";
+
+            return result;
+        }
+
+    }
+}
---
+++ HOT Labs/Tutorial/Samples/MemoryAddress.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial.Samples
+{
+    public class MemoryAddress
+    {
+        public short Base10Value { get; private set; }
+        public string HexValue
+        {
+            get
+            {
+                string hex = "0x";
+                // A short number in hexadecimal 
+                // will be at most 4 digits
+                //    FFFF
+                //    ||||
+                //    |||- 16^0 =>    1
+                //    ||-- 16^1 =>   16
+                //    |--- 16^2 =>  256
+                //    ---- 16^3 => 4096
+                int value = Base10Value;
+                int portion = value / 4096;
+                hex += ToHexDigit(portion);
+                value -= portion;
+                portion = value / 256;
+                hex += ToHexDigit(portion);
+                value -= portion;
+                portion = value / 16;
+                hex += ToHexDigit(portion);
+                portion = value % 16;
+                hex += ToHexDigit(portion);
+                return hex;
+            }
+        }
+
+        public MemoryAddress(short address)
+        {
+            Base10Value = address;
+        }
+
+        private static string ToHexDigit(int number)
+        {
+            string result;
+            if (number < 10)
+                result = number.ToString();
+            else if (number == 10)
+                result = "A";
+            else if (number == 11)
+                result = "B";
+            else if (number == 12)
+                result = "C";
+            else if (number == 13)
+                result = "D";
+            else if (number == 14)
+                result = "E";
+            else if (number == 15)
+                result = "F";
+            else // Should never happen...
+                result = "";
+
+            return result;
+        }
+    }
+}
