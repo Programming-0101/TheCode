62836beb6d8e1894a8a647627ef6b19b2b4d4047
dagilleland <dagilleland@shaw.ca>
2017-11-11 03:41:06 -0700

    improve tests for E.Calculator

--- HOT Topics/Topic/E/Examples/Specs/E1_Calculator.cs
+++ HOT Topics/Topic/E/Examples/Specs/E1_Calculator.cs
@@ -7,23 +7,27 @@ namespace Topic.E.Examples.Specs
 {
     public abstract class E1_Calculator : ReflectionBase
     {
-        [Fact]
+        [Theory]
         [Trait("New Tests", "Topic E Calculator Example")]
-        public void Should_Add_Two_Numbers()
+        [InlineData(4, 3, 7)]
+        [InlineData(5, 7, 12)]
+        public void Should_Add_Two_Numbers(int first, int second, int expected)
         {
             // Invoke a static method on a type
             var sut = Type.GetType(TypeName, true);
-            var actual = sut.GetMethod("Add", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static).Invoke(null, new object[] { 5, 7 });
-            Assert.Equal(12, actual);
+            var actual = sut.GetMethod("Add", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static).Invoke(null, new object[] { first, second });
+            Assert.True(expected.Equals(actual), $"Expected {first} + {second} to be {expected}, but got {actual}");
         }
 
-        [Fact]
+        [Theory]
         [Trait("New Tests", "Topic E Calculator Example")]
-        public void Should_Multiply_Two_Numbers()
+        [InlineData(4, 3, 12)]
+        [InlineData(5, 7, 35)]
+        public void Should_Multiply_Two_Numbers(int first, int second, int expected)
         {
             var sut = Type.GetType("Topic.E.Examples.Calculator,Topic.xUnit.Specs", true);
-            var actual = sut.GetMethod("Multiply", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static).Invoke(null, new object[] { 4, 3 });
-            Assert.Equal(12, actual);
+            var actual = sut.GetMethod("Multiply", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static).Invoke(null, new object[] { first, second });
+            Assert.True(expected.Equals(actual), $"Expected {first} * {second} to be {expected}, but got {actual}");
         }
     }
 }
